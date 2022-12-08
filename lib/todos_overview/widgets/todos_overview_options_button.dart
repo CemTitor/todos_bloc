f294ce73c0eaaa19bef78dff74dd328d29971a6c
CemYilmaz <cemyilmaz463@gmail.com>
2022-12-08 18:32:49 +0300

    Added Todos Overview Widget

---
+++ lib/todos_overview/widgets/todos_overview_options_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todos_bloc/l10n/l10n.dart';
+import 'package:todos_bloc/todos_overview/todos_overview.dart';
+
+@visibleForTesting
+enum TodosOverviewOption { toggleAll, clearCompleted }
+
+///exposes two options for manipulating todos:
+/// - toggleAll
+/// - clearCompleted
+class TodosOverviewOptionsButton extends StatelessWidget {
+  const TodosOverviewOptionsButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    final todos = context.select((TodosOverviewBloc bloc) => bloc.state.todos);
+    final hasTodos = todos.isNotEmpty;
+    final completedTodosAmount = todos.where((todo) => todo.isCompleted).length;
+
+    return PopupMenuButton<TodosOverviewOption>(
+      shape: const ContinuousRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      tooltip: l10n.todosOverviewOptionsTooltip,
+      onSelected: (options) {
+        switch (options) {
+          case TodosOverviewOption.toggleAll:
+            context
+                .read<TodosOverviewBloc>()
+                .add(const TodosOverviewToggleAllRequested());
+            break;
+          case TodosOverviewOption.clearCompleted:
+            context
+                .read<TodosOverviewBloc>()
+                .add(const TodosOverviewClearCompletedRequested());
+        }
+      },
+      itemBuilder: (context) {
+        return [
+          PopupMenuItem(
+            value: TodosOverviewOption.toggleAll,
+            enabled: hasTodos,
+            child: Text(
+              completedTodosAmount == todos.length
+                  ? l10n.todosOverviewOptionsMarkAllIncomplete
+                  : l10n.todosOverviewOptionsMarkAllComplete,
+            ),
+          ),
+          PopupMenuItem(
+            value: TodosOverviewOption.clearCompleted,
+            enabled: hasTodos && completedTodosAmount > 0,
+            child: Text(l10n.todosOverviewOptionsClearCompleted),
+          ),
+        ];
+      },
+      icon: const Icon(Icons.more_vert_rounded),
+    );
+  }
+}
